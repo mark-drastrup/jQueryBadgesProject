9c73abe4deb5b7ccf414b8020aa2a35c7dfbad33
mark_drastrup <mark.drastrup@gmail.com>
2017-10-12 17:55:26 +0200

    Fifth try

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,11 +4,11 @@ $(function() {
     url: "https://www.codeschool.com/users/markdrastrup.json",
     dataType: "jsonp",
     success: function(response) {
-        var badgeElements= $.map(response.courses.completed, function(item, index) {
+        var badgeElements = $.map(response, function(item, index) {
             var badgeDiv = $('<div />', {"class": "course"});
             $('<h3>'+item.title+'</h3>').appendTo(badgeDiv);
             $('<img />', {src: item.badge}).appendTo(badgeDiv);
-            $('<a />', {class: "btn btn-primary", target: "_blank", href: item.course.url}).text("See Course").appendTo(badgeDiv);
+            $('<a />', {"class": "btn btn-primary", target: "_blank", href: item.course.url}).text("See Course").appendTo(badgeDiv);
             return badgeDiv;
         })
         $("#badges").html(badgeElements);
