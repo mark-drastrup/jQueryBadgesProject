fe014298e8337da3c7deacedd32a8784a878fbbf
mark_drastrup <mark.drastrup@gmail.com>
2017-10-12 17:28:49 +0200

    First try

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+    url: "https://www.codeschool.com/users/markdrastrup.json",
+    dataType: "jsonp",
+    succes: function(response) {
+        var badgeElements= $.map(response.courses.completed, function(item, index) {
+            var badgeDiv = $('<div />', {class: "course"});
+            $('<h3>'+response.courses.completed.title+'</h3>').appendTo(badgeDiv);
+            $('<img />', {src: response.courses.completed.badge}).appendTo(badgeDiv);
+            $('<a />', {class: "btn btn-primary", target: "_blank", href: response.courses.completed.course.url}).text("See Course").appendTo(badgeDiv);
+            return badgeDiv;
+        })
+        $("#badges").html(badgeElements);
+    }
+  })
 });
+
